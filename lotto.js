f13dc302e2c5e5898739be5eded56e7f5c8872a8
christy <vennesach@gmail.com>
2023-04-12 14:20:08 +0800

    完成樂透demo

--- lotto.js
+++ lotto.js
@@ -1,27 +1,19 @@
-const element = document.querySelector("h1");
-console.log(element);
-
-// 更改本文
-element.innerText = element.innerText;
-// 更改HTML
-element.innerHTML = `<li style="color:blue">${element.innerText}</li>`;
-
-//<p class="date">
-const deteE1 = document.querySelector("p.date");
-console.log(deteE1);
-
-//<h3>下面的<span class="date">
-const deteE2 = document.querySelector("span.date"); // 同document.querySelector("h3 .date")
-deteE2.innerText = "2023/4/6";
-console.log(deteE2);
-
 const dateE1s = document.querySelectorAll(".date");
 console.log(dateE1s);
 dateE1s.forEach(x => x.innerText = getToday());
-// //最上方的日期顯示, 改變為空字串
-// dateE1s[0].innerHTML = "";
-//最上方的日期顯示, 移除
-dateE1s[0].remove();
+dateE1s[0].setAttribute("style","color:black;fontsize=16");
+showTime()
+
+click=false;
+function showTime() {
+    dateE1s[0].innerText = getTime(); 
+    setTimeout(() => {
+        showTime();
+        if (!click) {
+            lottoClick();
+        }
+    },1000);
+}
 
 // 取得今天日期
 function getToday() {
@@ -33,24 +25,53 @@ function getToday() {
     return `${year}-${month}-${date}`;
 }
 
-const startE1 = document.querySelector("#start");
-console.log(startE1);
-startE1.innerText = ("開獎!");
+//取得時、分、秒跟年、月、日
+function getTime(fullTime = true) {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth()+1;
+    let date = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+    
+    if (fullTime) {
+        return `${year}/${month}/${date} ${hours}:${minutes}:${seconds}`;
+    }
+    
+    return `${year}/${month}/${date}`;
+}
+// const startE1 = document.querySelector("#start");
+// console.log(startE1);
+// startE1.innerText = ("開獎!");
 
-startE1.addEventListener("click", lottoClick);
+// startE1.addEventListener("click", lottoClick);
 
 function lottoClick() {
     let lottos = [];
-    const lottoE1 = document.querySelector(".lotto-number ul");
+    click=true;
+    const lottoE1 = document.querySelector(".lotto-number");
     lottoE1.innerHTML = "";
     for(let i = 0; i < 5; i++) {
         let lotto = getLotto(start, end);
         lottos.push(lotto);
     }   
     
-    for (let i = 0; i < lottos.length; i++) {    
-        lottoE1.innerHTML+= `<li style=${i % 2 == 0 ? "color:red" : "color:blue"} > ${lottos[i].join(",")}</li>`;
-
+    console.log(lottos);
+    let htmlText="<table border = 1>";
+    for (let i = 0; i < lottos.length; i++) {
+        htmlText += "<tr>";
+        for(let j = 0; j < lottos[i].length; j++) {
+            htmlText += `<td>${[lottos[i][j]]}</td>`;
+        }
+        htmlText += "</tr>";
     }
+    htmlText+="</table>";
+
+    console.log(htmlText);
+    lottoE1.innerHTML = htmlText;
+    // for (let i = 0; i < lottos.length; i++) {    
+    //     lottoE1.innerHTML+= `<li style=${i % 2 == 0 ? "color:black" : "color:blue"} > ${lottos[i].join(",")}</li>`;
+    // }
 }
 
